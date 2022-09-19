9166ddf81176f0fdd81f24b694763a6abd5cedbb
Anton Yushkov <yuschkof.toscha@yandex.ru>
2022-09-19 20:09:10 +0400

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LearningCHarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+        
+        static void Start() {
+            Console.WriteLine("Шалом");
+        }
+    }
+}
